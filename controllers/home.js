528c54cdf30c1649b045b0de343e1e96da542e0b
Jason Malcolm <jaciones@hotmail.com>
2011-07-18 16:10:12 +0000

    Tweak

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,13 @@
-require 'http'
+/*var connect = require('connect');   
+var url= require('url')
+var MemoryStore = require('connect/middleware/session/memory');
+var OAuth= require('oauth').OAuth;
+var oa= new OAuth("http://localhost:3000/oauth/request_token",
+                 "http://localhost:3000/oauth/access_token", 
+                 "JiYmll7CX3AXDgasnnIDeg",  "mWPBRK5kG2Tkthuf5zRV1jYWOEwnjI6xs3QVRqOOg", 
+                 "1.0A", "http://localhost:4000/oauth/callback", "HMAC-SHA1");       
+
+*/
 
 app.get('/', function(req, res){
   res.render('index', {
@@ -7,6 +16,9 @@ app.get('/', function(req, res){
   });
 });
 
-app.get('/connect', function(req, res){
-
+app.get('/callback', function(req, res){
+  res.render('index', {
+    title: 'Express',
+    bar: 'Bar'
+  });
 });
