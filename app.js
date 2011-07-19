7b9ef13d001b3074272ee0f82b15eded9511cc67
Jason Malcolm <jaciones@hotmail.com>
2011-07-19 17:09:09 +0000

    Got 4square working

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var db = mongoose.connect('mongodb://localhost/foo');
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'jade');
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
+   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
--- controllers/home.js
+++ controllers/home.js
@@ -1,21 +1,22 @@
 var config = {
    "secrets": {
-      "clientId": "CLIENT_ID",
-      "clientSecret": "CLIENT_SECRET",
-      "redirectUrl": "REDIRECT_URL"
+      "clientId": "A1NL3FU1D1QS2SH5YCXQX5D4Y1NMQUXRKV0HZOBBIADI4CBH",
+      "clientSecret": "UEFWAG4WSEAY5S3QIN1AS3ULF30HAZZGEOEGHCG0N2Z0AMLS",
+      "redirectUrl": "http://jaciones_test_repository.jaciones.cloud9ide.com/callback"
    }
 }
 
-var foursquare = require("node-foursquare")(config);
+var Foursquare = require("node-foursquare")(config);
 
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express',
-    bar: 'Bar'
-  });
+app.get('/', function(req, res) {
+   res.render('index', {
+      title: 'Express',
+      bar: 'Bar'
+   });
 });
 
 app.get('/login', function(req, res) {
+   console.log("login");
    res.writeHead(303, {
       "location": Foursquare.getAuthClientRedirectUrl()
    });
@@ -23,6 +24,7 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/callback', function(req, res) {
+   console.log("Callback");
    Foursquare.getAccessToken({
       code: req.query.code
    }, function(error, accessToken) {
@@ -30,6 +32,10 @@ app.get('/callback', function(req, res) {
          res.send("An error was thrown: " + error.message);
       }
       else {
+   res.writeHead(303, {
+      "location": "/home"
+   });
+   res.end(); 
          // Save the accessToken and redirect.
       }
    });
