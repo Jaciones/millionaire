664b2bc9eb35fd3cc6e902fe5ff7a1f4c15d0e1c
Jason Malcolm <jaciones@hotmail.com>
2011-08-01 01:28:20 +0000

    Updated venue stuff

--- models/multiplierTypes.js
+++ models/multiplierTypes.js
@@ -2,9 +2,9 @@ MultiplierTypes = {
    houses: [{
       id: 'house0',
       name: 'Beginner House',
-      desc: 'Beginner House',
-      mini_desc: '+ %5',
-      photo: '',
+      desc: 'This fixer-upper is the perfect spot to begin for the aspiring real estate mogul',
+      mini_desc: '+ 5% to rent',
+      photo: '/images/mults/house0',
       cost: 100,
       func: function(venue, user) {
          return venue.value * 0.05;
@@ -12,30 +12,30 @@ MultiplierTypes = {
    }, {
       id: 'house1',
       name: 'Small House',
-      desc: 'Small House',
-      mini_desc: '+ %10',
-      photo: '',
+      desc: 'A small house is better than no house at all, right?',
+      mini_desc: '+ 10% to rent',
+      photo: '/images/mults/house1',
       cost: 200,
       func: function(venue, user) {
          return venue.value * 0.10;
       }
    }, {
-      id: 'house3',
-      name: 'House',
-      desc: 'House',
-      mini_desc: '+ %15',
-      photo: '',
-      cost: 300,
+      id: 'house2',
+      name: 'Decent House',
+      desc: 'This is a home any working class owner would be proud to call their own.',
+      mini_desc: '+ 15% to rent',
+      photo: '/images/mults/house2',
+      cost: 500,
       func: function(venue, user) {
          return venue.value * 0.15;
       }
    }, {
       id: 'house4',
       name: 'Nice House',
-      desc: 'Nice House',
-      mini_desc: '+ %20',
-      photo: '',
-      cost: 400,
+      desc: 'This house shows your friends that you are really working your way up in the world!',
+      mini_desc: '+ 20% to rent',
+      photo: '/images/mults/mansion',
+      cost: 10000,
       func: function(venue, user) {
          return venue.value * 0.20;
       }
@@ -43,9 +43,9 @@ MultiplierTypes = {
    buildings: [{
       id: 'facebook_shop',
       name: 'Facebook Shop',
-      desc: 'Facebook Shop',
-      mini_desc: '+ %15',
-      photo: '',
+      desc: 'Show your friends how wealthy you are becoming by sharing you success on Facebook. This house is free with a wall-post to Facebook',
+      mini_desc: '+ 15% to rent',
+      photo: '/images/mults/facebook',
       cost: 0,
       func: function(venue, user) {
          return venue.value * 0.15;
@@ -53,9 +53,9 @@ MultiplierTypes = {
    }, {
       id: 'twitter_shop',
       name: 'Twitter Shop',
-      desc: 'Twitter Shop',
-      mini_desc: '+ %15',
-      photo: '',
+      desc: 'Show your friends how wealthy you are becoming by sharing you success on Twitter. This house is free with a tweet on Twitter',
+      mini_desc: '+ 15% to rent',
+      photo: '/images/mults/twitter',
       cost: 0,
       func: function(venue, user) {
          return venue.value * 0.15;
@@ -63,9 +63,9 @@ MultiplierTypes = {
    }, {
       id: 'shop0',
       name: 'Simple Shop',
-      desc: 'Simple Shop',
-      mini_desc: '+ $200',
-      photo: '',
+      desc: 'Even the smallest shop owner makes money.',
+      mini_desc: '+ $200 to rent',
+      photo: '/images/mults/coffee_store',
       cost: 1000,
       func: function(venue, user) {
          return 200;
@@ -73,9 +73,9 @@ MultiplierTypes = {
    }, {
       id: 'shop1',
       name: 'Shop',
-      desc: 'Shop',
-      mini_desc: '+ $450',
-      photo: '',
+      desc: 'People have been hearing about your success as a business man.',
+      mini_desc: '+ $450 to rent',
+      photo: '/images/mults/record_store',
       cost: 2000,
       func: function(venue, user) {
          return 450;
@@ -83,14 +83,25 @@ MultiplierTypes = {
    }, {
       id: 'shop2',
       name: 'Fancy Shop',
-      desc: 'Fancy Shop',
-      mini_desc: '+ $1000',
-      photo: '',
+      desc: 'People have been hearing about your success as a business man.',
+      mini_desc: '+ $1000 to rent',
+      photo: '/images/mults/pharmacy',
       cost: 4000,
       func: function(venue, user) {
          return 1000;
       }
-   }],
+   }, {
+      id: 'large_business',
+      name: 'Large Business',
+      desc: 'Your wealth is growing and it\'s about time to expand',
+      mini_desc: '+ $2000 to rent',
+      photo: '/images/mults/large_business',
+      cost: 8000,
+      func: function(venue, user) {
+         return 1000;
+      }      
+   }
+   ],
    all: function() {
       return MultiplierTypes.houses.concat(MultiplierTypes.buildings);
    },
